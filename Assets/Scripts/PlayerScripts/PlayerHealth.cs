772a4380a16c259f7308f26b5ee6d307915d4756
Andrademin <alexamaroandrade.2007@gmail.com>
2026-06-17 04:10:18 +0100

    Attempted too fix heavy problems

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -153,4 +153,9 @@ public class PlayerHealth : NetworkBehaviour
             FallDeath();
         }
     }
+
+    public void SetPlayerIndex(int index)
+    {
+        _playerIndex = index;
+    }
 }
